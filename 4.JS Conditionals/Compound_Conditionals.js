d401f94ebf8c8f282d26457d013344900fe88cd8
ShreeP2020 <shreeyogeshpatil2020@gmail.com>
2020-11-22 12:28:17 +0100

    Initial commit

---
+++ 4.JS Conditionals/Compound_Conditionals.js
@@ -0,0 +1,19 @@
+var age = 11;
+
+if (age <= 10) {
+  console.log("You can eat from the kid's menu");
+  console.log("You are not old enough to drive");
+  console.log("You are not old enough to rent a car");
+} else if (age >= 16 && age < 25) {
+  console.log("You can not eat from the kid's menu");
+  console.log("You are old enough to drive");
+  console.log("You are not old enough to rent a car");
+} else if (age >= 25) {
+  console.log("You can not eat from the kid's menu");
+  console.log("You are old enough to drive");
+  console.log("You are old enough to rent a car");
+} else {
+  console.log("You can not eat from the kid's menu");
+  console.log("You are not old enough to drive");
+  console.log("You are not old enough to rent a car");
+}
