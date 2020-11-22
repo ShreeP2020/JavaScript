86dede2498aaf37bab3fc530ab779666c5d739cc
ShreeP2020 <shreeyogeshpatil2020@gmail.com>
2020-11-22 12:23:53 +0100

    Initial commit

---
+++ 1.JS Data types/Comprehensive_List_of_JavaScript_Data_Types.js
@@ -0,0 +1,24 @@
+typeof 12;
+// "number"
+console.log(typeof 12);
+
+typeof 'Astros';
+// "string"
+
+typeof true;
+// "boolean"
+
+typeof { name: "Kristine" };
+// "object"
+
+var age;
+// undefined
+
+typeof age;
+// "undefined"
+
+age = null;
+// null
+
+typeof age;
+// "object"
