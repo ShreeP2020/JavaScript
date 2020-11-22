9005efce5f50aa5f3bf512e4910f1a4365ed0e76
ShreeP2020 <shreeyogeshpatil2020@gmail.com>
2020-11-22 12:27:39 +0100

    Initial commit

---
+++ 3.JS Operators/Compound_Assignment_operators.js
@@ -0,0 +1,17 @@
+var name = 'Tiffany';
+
+var sum = 0;
+
+var gradeOne = 100;
+
+var gradeTwo = 80;
+
+sum += gradeOne; // 100
+
+sum; // 100
+
+sum += gradeTwo; // 180
+console.log('sum', sum);
+
+sum *= gradeTwo; // 14400
+console.log('sum', sum);
