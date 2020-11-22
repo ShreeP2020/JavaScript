5ae186001d2728be6fca1c0febf8e7d53a8a7941
ShreeP2020 <shreeyogeshpatil2020@gmail.com>
2020-11-22 12:26:33 +0100

    Initial commit

---
+++ 2.String Functions/part3.js
@@ -0,0 +1,19 @@
+var str = 'The quick brown fox jumped over the lazy dog';
+
+str.slice(4, 10); // "quick "
+
+str.slice(-8); // "lazy dog"
+
+str.slice(10); // "brown fox jumped over the lazy dog"
+
+str.toUpperCase(); // "THE QUICK BROWN FOX JUMPED OVER THE LAZY DOG"
+
+str.toLowerCase(); // "the quick brown fox jumped over the lazy dog"
+
+var messyString = '    Hi there    ';
+
+messyString.trim(); // "Hi there"
+
+console.log(str.slice(10));
+console.log(str.toLowerCase());
+console.log(messyString.trim());
